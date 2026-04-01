5c29bb28252c7ab001b3f4e422221c06f8fca220
lelouchZhang <zhch8128@gmail.com>
2026-04-01 16:33:35 +0800

    feat: 添加书籍卡片和示例数据

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
+import BookCard from "@/components/BookCard";
 import Hero from "@/components/hero";
+import { sampleBooks } from "@/lib/constants";
 
 const Home = () => {
   return (
     <main className="wrapper container">
       <Hero />
+      <div className="library-books-grid">
+        {sampleBooks.map((book) => (
+          <BookCard
+            key={book._id}
+            title={book.title}
+            author={book.author}
+            coverURL={book.coverURL}
+            slug={book.slug}
+          />
+        ))}
+      </div>
     </main>
   );
 };
