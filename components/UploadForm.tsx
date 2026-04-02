4e8fd650142a21d78216fa9f4ef89d5a09c4f6e1
lelouchZhang <zhch8128@gmail.com>
2026-04-02 16:12:26 +0800

    feat: 添加上传加载遮罩

--- components/UploadForm.tsx
+++ components/UploadForm.tsx
@@ -15,13 +15,15 @@ import { Controller, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { BookUploadFormValues } from "@/types";
 import { UploadSchema } from "@/lib/zod";
-import FileUploader from "./FileUploader";
 import { ACCEPTED_IMAGE_TYPES, ACCEPTED_PDF_TYPES } from "@/lib/constants";
 import { ImageIcon, Upload } from "lucide-react";
-import { toast } from "sonner";
 import { useEffect, useState } from "react";
-import VoiceSelector from "./VoiceSelector";
 import { Button } from "./ui/button";
+import { toast } from "sonner";
+
+import FileUploader from "./FileUploader";
+import LoadingOverlay from "./LoadingOverlay";
+import VoiceSelector from "./VoiceSelector";
 
 const UploadForm = () => {
   const [isSubmitting, setIsSubmitting] = useState(false);
@@ -47,106 +49,117 @@ const UploadForm = () => {
     //   return toast.error("Please login to upload books");
     // }
     setIsSubmitting(true);
+    console.log(data);
   };
 
   if (!isMounted) return null;
 
   return (
-    <div className="new-book-wrapper">
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        {/* PDF Uploader */}
-        <FileUploader
-          control={form.control}
-          name="pdfFile"
-          label="Upload PDF File"
-          acceptTypes={ACCEPTED_PDF_TYPES}
-          icon={Upload}
-          placeholder="Click to upload PDF"
-          hint="PDF file (max 50MB)"
-          disabled={isSubmitting}
-        />
-        {/* image Uploader */}
-        <FileUploader
-          control={form.control}
-          name={"coverImage"}
-          label={"Upload Cover Image"}
-          acceptTypes={ACCEPTED_IMAGE_TYPES}
-          icon={ImageIcon}
-          placeholder={"Click to upload cover image"}
-          hint={"Image file (max 50MB)"}
-          disabled={isSubmitting}
-        />
-        {/* Title using Field */}
-        <Controller
-          name="title"
-          control={form.control}
-          render={({ field, fieldState }) => (
-            <Field data-invalid={fieldState.invalid}>
-              <FieldLabel className="form-label" htmlFor="title">
-                Title
-              </FieldLabel>
-              <FieldContent>
-                <input
-                  id="title"
-                  type="text"
-                  className="form-input"
-                  placeholder="ex: Rich Dad Poor Dad"
-                  {...field}
-                  disabled={isSubmitting}
-                />
-              </FieldContent>
-              {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
-            </Field>
-          )}
-        />
-        {/* Author using Field*/}
-        <Controller
-          control={form.control}
-          name="author"
-          render={({ field, fieldState }) => (
-            <Field data-invalid={fieldState.invalid}>
-              <FieldLabel className="form-label" htmlFor="author">
-                Author Name
-              </FieldLabel>
-              <FieldContent>
-                <input
-                  id="author"
-                  type="text"
-                  className="form-input"
-                  placeholder="ex: Robert Kiyosaki"
-                  {...field}
-                  disabled={isSubmitting}
-                />
-              </FieldContent>
-              {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
-            </Field>
-          )}
-        />
-        {/* 5. Voice Selector */}
-        <Controller
-          control={form.control}
-          name="persona"
-          render={({ field, fieldState }) => (
-            <Field data-invalid={fieldState.invalid}>
-              <FieldLabel className="form-label" htmlFor="author">
-                Choose Assistant Voice
-              </FieldLabel>
-              <VoiceSelector
-                value={field.value}
-                onChange={field.onChange}
-                disabled={isSubmitting}
-              />
+    <>
+      {isSubmitting && <LoadingOverlay />}
 
-              {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
-            </Field>
-          )}
-        />
-        {/* submit button */}
-        <Button type="submit" className="form-btn" disabled={isSubmitting}>
-          Begin Synthesis
-        </Button>
-      </form>
-    </div>
+      <div className="new-book-wrapper">
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          {/* PDF Uploader */}
+          <FileUploader
+            control={form.control}
+            name="pdfFile"
+            label="Upload PDF File"
+            acceptTypes={ACCEPTED_PDF_TYPES}
+            icon={Upload}
+            placeholder="Click to upload PDF"
+            hint="PDF file (max 50MB)"
+            disabled={isSubmitting}
+          />
+          {/* image Uploader */}
+          <FileUploader
+            control={form.control}
+            name={"coverImage"}
+            label={"Upload Cover Image"}
+            acceptTypes={ACCEPTED_IMAGE_TYPES}
+            icon={ImageIcon}
+            placeholder={"Click to upload cover image"}
+            hint={"Image file (max 50MB)"}
+            disabled={isSubmitting}
+          />
+          {/* Title using Field */}
+          <Controller
+            name="title"
+            control={form.control}
+            render={({ field, fieldState }) => (
+              <Field data-invalid={fieldState.invalid}>
+                <FieldLabel className="form-label" htmlFor="title">
+                  Title
+                </FieldLabel>
+                <FieldContent>
+                  <input
+                    id="title"
+                    type="text"
+                    className="form-input"
+                    placeholder="ex: Rich Dad Poor Dad"
+                    {...field}
+                    disabled={isSubmitting}
+                  />
+                </FieldContent>
+                {fieldState.invalid && (
+                  <FieldError errors={[fieldState.error]} />
+                )}
+              </Field>
+            )}
+          />
+          {/* Author using Field*/}
+          <Controller
+            control={form.control}
+            name="author"
+            render={({ field, fieldState }) => (
+              <Field data-invalid={fieldState.invalid}>
+                <FieldLabel className="form-label" htmlFor="author">
+                  Author Name
+                </FieldLabel>
+                <FieldContent>
+                  <input
+                    id="author"
+                    type="text"
+                    className="form-input"
+                    placeholder="ex: Robert Kiyosaki"
+                    {...field}
+                    disabled={isSubmitting}
+                  />
+                </FieldContent>
+                {fieldState.invalid && (
+                  <FieldError errors={[fieldState.error]} />
+                )}
+              </Field>
+            )}
+          />
+          {/* 5. Voice Selector */}
+          <Controller
+            control={form.control}
+            name="persona"
+            render={({ field, fieldState }) => (
+              <Field data-invalid={fieldState.invalid}>
+                <FieldLabel className="form-label" htmlFor="persona">
+                  Choose Assistant Voice
+                </FieldLabel>
+                <VoiceSelector
+                  value={field.value}
+                  onChange={field.onChange}
+                  disabled={isSubmitting}
+                />
+
+                {fieldState.invalid && (
+                  <FieldError errors={[fieldState.error]} />
+                )}
+              </Field>
+            )}
+          />
+          {/* submit button */}
+          <Button type="submit" className="form-btn" disabled={isSubmitting}>
+            Begin Synthesis
+          </Button>
+        </form>
+      </div>
+    </>
   );
 };
 
