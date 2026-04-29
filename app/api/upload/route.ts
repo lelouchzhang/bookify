80cfa835c66a87b332b9f64a57ddb116982eeebe
lelouchZhang <zhch8128@gmail.com>
2026-04-30 00:12:07 +0800

    fixes

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -4,9 +4,8 @@ import { handleUpload, HandleUploadBody } from "@vercel/blob/client";
 import { NextResponse } from "next/server";
 
 export const POST = async (request: Request): Promise<NextResponse> => {
-  const body = (await request.json()) as HandleUploadBody;
-
   try {
+    const body = (await request.json()) as HandleUploadBody;
     const jsonResponse = await handleUpload({
       body: body,
       request: request,
